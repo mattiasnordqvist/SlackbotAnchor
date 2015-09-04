b8403f65615acc05a52f108e7978461913c00eaa
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2015-09-04 19:20:49 +0200

    Cleanup
    Moved code from Source to SlackbotAnchor
    Placed solution-file outside project folder
    Created nuspec file

---
+++ SlackbotAnchor/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Slackbot Anchor")]
+[assembly: AssemblyDescription("Access your slackbot from .Net. Not created by, affiliated with, or supported by Slack Technologies, Inc.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Slackbot Anchor")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6950db62-9f07-43cd-81ec-be2a900f940e")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
