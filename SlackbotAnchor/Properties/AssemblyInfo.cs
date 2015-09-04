aa3cac58ac48c8ed2a4cc01eb066470cf23a654d
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2015-09-04 19:39:06 +0200

    Update assembly file

--- SlackbotAnchor/Properties/AssemblyInfo.cs
+++ SlackbotAnchor/Properties/AssemblyInfo.cs
@@ -5,14 +5,15 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Slackbot Anchor")]
 [assembly: AssemblyDescription("Access your slackbot from .Net. Not created by, affiliated with, or supported by Slack Technologies, Inc.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mattias Nordqvist")]
+[assembly: AssemblyCopyright("Copyright © Mattias Nordqvist")]
 [assembly: AssemblyProduct("Slackbot Anchor")]
-[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: ComVisible(false)]
+
 [assembly: Guid("6950db62-9f07-43cd-81ec-be2a900f940e")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
