a1e5b35ee6aaa708f10ee3b8b22b3e64302c9b27
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2015-09-04 19:54:11 +0200

    Fixed copy-paste errors in nuspec

--- SlackbotAnchor/Properties/AssemblyInfo.cs
+++ SlackbotAnchor/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Slackbot Anchor")]
@@ -16,5 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("6950db62-9f07-43cd-81ec-be2a900f940e")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
